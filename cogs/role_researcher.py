3dca31e36a615ff370945a71a4534d7609b65ed6
jily64 <108062664+jily64@users.noreply.github.com>
2023-10-13 12:02:03 +0300

    Update

--- cogs/role_researcher.py
+++ cogs/role_researcher.py
@@ -4,8 +4,125 @@ from discord.ext import tasks
 from discord import app_commands
 import json
 import asyncio
+import time
 
 class on_roles_check(commands.Cog):
+
+    @tasks.loop(hours=1)
+    async def sdsf(self, bot):
+
+            print("--[Roles Researcher Logs]: Pre Loading Values")
+            try:
+                self.mess = await self.channel.fetch_message(self.data["MessID"])
+                func = self.mess.edit
+            except:
+                func = self.channel.send
+            data = self.data
+            for i in data:
+                if i != "MessID":
+                    data[i]["member"] = []
+                    for j in data[i]["role"].members:
+                        data[i]["member"].append(j.mention)
+                    if data[i]["member"] == []:
+                        data[i]["member"].append("Не назначен")
+            try:
+                guild = self.bot.get_guild(1096100154148397208)
+                mods = ""
+                for i in range(len(data["Mod"]["member"])):
+                    mods += f'{data["Mod"]["member"][i]} \n'
+
+                Sys = ""
+                for i in range(len(data["Sys"]["member"])):
+                    Sys += f'{data["Sys"]["member"][i]} \n'
+
+                MlSys = ""
+                for i in range(len(data["MlSys"]["member"])):
+                    MlSys += f'{data["MlSys"]["member"][i]} \n'
+
+                GameRup = ""
+                for i in range(len(data["GameRup"]["member"])):
+                    GameRup += f'{data["GameRup"]["member"][i]} \n'
+
+                GameEve = ""
+                for i in range(len(data["GameEve"]["member"])):
+                    GameEve += f'{data["GameEve"]["member"][i]} \n'
+
+                print("--[Roles Researcher Logs]: Compiling Message")
+
+                message = (f'>>> # Персонал сервера: \n'
+                           f'## Модерация \n'
+                           f'\n'
+                           f'{data["CatMod"]["role"].mention} \n'
+                           f'\n'
+                           f'{data["SeM"]["role"].mention} \n'
+                           f'{data["SeM"]["member"][0]} \n'
+                           f'\n'
+                           f'{data["ZamSeM"]["role"].mention} \n'
+                           f'{data["ZamSeM"]["member"][0]} \n'
+                           f'\n'
+                           f'{data["StM"]["role"].mention} \n'
+                           f'{data["StM"]["member"][0]} \n'
+                           f'\n'
+                           f'{data["Sec"]["role"].mention} \n'
+                           f'{data["Sec"]["member"][0]} \n'
+                           f'\n'
+                           f'{data["Mod"]["role"].mention} \n'
+                           f'{mods}'
+                           f'\n'
+                           f'## Cисадмины \n'
+                           f'\n'
+                           f'{data["CatSys"]["role"].mention}\n'
+                           f'\n'
+                           f'{data["SeSys"]["role"].mention} \n'
+                           f'{data["SeSys"]["member"][0]} \n'
+                           f'\n'
+                           f'{data["Sys"]["role"].mention} \n'
+                           f'{Sys}'
+                           f'\n'
+                           f'{data["MlSys"]["role"].mention} \n'
+                           f'{MlSys}'
+                           f'\n'
+                           f'## Креатив Отдел \n'
+                           f'\n'
+                           f'{data["CatCr"]["role"].mention} \n'
+                           f'\n'
+                           f'{data["SeCr"]["role"].mention} \n'
+                           f'{data["SeCr"]["member"][0]} \n'
+                           f'\n'
+                           f'## Игро-Фыры \n'
+                           f'\n'
+                           f'{data["CatGame"]["role"].mention}\n'
+                           f'\n'
+                           f'{data["SeGame"]["role"].mention} \n'
+                           f'{data["SeGame"]["member"][0]} \n'
+                           f'\n'
+                           f'{data["Primet"]["role"].mention} \n'
+                           f'{data["Primet"]["member"][0]} \n'
+                           f'\n'
+                           f'{data["GameRup"]["role"].mention} \n'
+                           f'{GameRup}'
+                           f'\n'
+                           f'{data["GameEve"]["role"].mention} \n'
+                           f'{GameEve}'
+                           f'\n'
+                           f'## Шефы серверов клана \n\n'
+                           f'{data["SeMine"]["role"].mention} \n'
+                           f'{data["SeMine"]["member"][0]} \n'
+                           f'\n'
+                           f'{data["SeNSFW"]["role"].mention} \n'
+                           f'{data["SeNSFW"]["member"][0]} \n'
+                           f'\n'
+                           f'{data["SeArt"]["role"].mention} \n'
+                           f'{data["SeArt"]["member"][0]} \n'
+                           f''
+                           )
+                print("--[Roles Researcher Logs]: Successfully Compiled! Sending...")
+                await func(content=message)
+                print("--[Roles Researcher Logs]: Successfully Sent!")
+            except:
+                channel = self.guild.get_channel(1149419053736272045)
+                self.mess = await channel.send("Preloaded Message \nIt will disappear in next 1 hour")
+
     def __init__(self, bot: commands.Bot):
         self.bot = bot
         self.data = {
@@ -27,7 +144,6 @@ class on_roles_check(commands.Cog):
                 "role": 1159359120864968744,
                 "member": []
             },
-
 
             "SeM": {
                 "role": 1159176342038921236,
@@ -108,129 +224,9 @@ class on_roles_check(commands.Cog):
         print(data)
         self.data = data
         self.channel = self.guild.get_channel(1149419053736272045)
-        #print(self.mess.id)
+        # print(self.mess.id)
         print("--[Roles Researcher Logs]: Roles and message are synced")
-
-
-    @tasks.loop(seconds=10)
-    async def task(self):
-        print("--[Roles Researcher Logs]: Pre Loading Values")
-        try:
-            self.mess = await self.channel.fetch_message(self.data["MessID"])
-            func = self.mess.edit
-        except:
-            func = self.channel.send
-        data = self.data
-        for i in data:
-            if i != "MessID":
-                data[i]["member"] = []
-                for j in data[i]["role"].members:
-                    data[i]["member"].append(j.mention)
-                if data[i]["member"] == []:
-                    data[i]["member"].append("Не назначен")
-        try:
-            guild = self.bot.get_guild(1096100154148397208)
-            mods = ""
-            for i in range(len(data["Mod"]["member"])):
-                mods += f'{data["Mod"]["member"][i]} \n'
-
-            Sys = ""
-            for i in range(len(data["Sys"]["member"])):
-                Sys += f'{data["Sys"]["member"][i]} \n'
-
-            MlSys = ""
-            for i in range(len(data["MlSys"]["member"])):
-                MlSys += f'{data["MlSys"]["member"][i]} \n'
-
-            GameRup = ""
-            for i in range(len(data["GameRup"]["member"])):
-                GameRup += f'{data["GameRup"]["member"][i]} \n'
-
-            GameEve = ""
-            for i in range(len(data["GameEve"]["member"])):
-                GameEve += f'{data["GameEve"]["member"][i]} \n'
-
-            print("--[Roles Researcher Logs]: Compiling Message")
-
-            message = (f'>>> # Персонал сервера: \n'
-                       f'## Модерация \n'
-                       f'\n'
-                       f'{data["CatMod"]["role"].mention} \n'
-                       f'\n'
-                       f'{data["SeM"]["role"].mention} \n'
-                       f'{data["SeM"]["member"][0]} \n'
-                       f'\n'
-                       f'{data["ZamSeM"]["role"].mention} \n'
-                       f'{data["ZamSeM"]["member"][0]} \n'
-                       f'\n'
-                       f'{data["StM"]["role"].mention} \n'
-                       f'{data["StM"]["member"][0]} \n'
-                       f'\n'
-                       f'{data["Sec"]["role"].mention} \n'
-                       f'{data["Sec"]["member"][0]} \n'
-                       f'\n'
-                       f'{data["Mod"]["role"].mention} \n'
-                       f'{mods}'
-                       f'\n'
-                       f'## Cисадмины \n'
-                       f'\n'
-                       f'{data["CatSys"]["role"].mention}\n'
-                       f'\n'
-                       f'{data["SeSys"]["role"].mention} \n'
-                       f'{data["SeSys"]["member"][0]} \n'
-                       f'\n'
-                       f'{data["Sys"]["role"].mention} \n'
-                       f'{Sys}'
-                       f'\n'
-                       f'{data["MlSys"]["role"].mention} \n'
-                       f'{MlSys}'
-                       f'\n'
-                       f'## Креатив Отдел \n'
-                       f'\n'
-                       f'{data["CatCr"]["role"].mention} \n'
-                       f'\n'
-                       f'{data["SeCr"]["role"].mention} \n'
-                       f'{data["SeCr"]["member"][0]} \n'
-                       f'\n'
-                       f'## Игро-Фыры \n'
-                       f'\n'
-                       f'{data["CatGame"]["role"].mention}\n'
-                       f'\n'
-                       f'{data["SeGame"]["role"].mention} \n'
-                       f'{data["SeGame"]["member"][0]} \n'
-                       f'\n'
-                       f'{data["Primet"]["role"].mention} \n'
-                       f'{data["Primet"]["member"][0]} \n'
-                       f'\n'
-                       f'{data["GameRup"]["role"].mention} \n'
-                       f'{GameRup}'
-                       f'\n'
-                       f'{data["GameEve"]["role"].mention} \n'
-                       f'{GameEve}'
-                       f'\n'
-                       f'## Шефы серверов клана \n\n'
-                       f'{data["SeMine"]["role"].mention} \n'
-                       f'{data["SeMine"]["member"][0]} \n'
-                       f'\n'
-                       f'{data["SeNSFW"]["role"].mention} \n'
-                       f'{data["SeNSFW"]["member"][0]} \n'
-                       f'\n'
-                       f'{data["SeArt"]["role"].mention} \n'
-                       f'{data["SeArt"]["member"][0]} \n'
-                       f''
-                       )
-            print("--[Roles Researcher Logs]: Successfully Compiled! Sending...")
-            await func(content=message)
-            print("--[Roles Researcher Logs]: Successfully Sent!")
-        except:
-            channel = self.guild.get_channel(1149419053736272045)
-            self.mess = await channel.send("Preloaded Message \nIt will disappear in next 1 hour")
-
-@tasks.loop(seconds=1)
-async def task(bot: commands.Bot):
-    guild = bot.get_guild(1096100154148397208)
-    channel = guild.get_channel(1149419032315969677)
-    await channel.send("Borislov")
+        self.sdsf.start(self.bot)
 
 async def setup(bot: commands.Bot):
     await bot.add_cog(on_roles_check(bot), guilds=bot.guilds)
