ed321f0b683101b6bd5558b3dead85fe12cecbee
Sergaris <84004494+Arti787@users.noreply.github.com>
2023-11-06 20:16:05 +0300

    Update main.py

--- main.py
+++ main.py
@@ -4,6 +4,7 @@ from load_config import load_config
 from context import Global
 import asyncio
 import os
+import json
 
 
 print(discord.__version__)
@@ -54,6 +55,41 @@ class CheeseHelper(commands.Bot):
         try:
             if message.channel.id in self.ctx.servers[message.guild.id]["read"]:
                 print(f"{message.author}: {message.content}")
+                """
+                # Your new code starts here
+                if not message.author.bot:
+                    # Read existing database
+                    if os.path.exists("users_db.json"):
+                        with open("users_db.json", "r") as f:
+                            user_db = json.load(f)
+                    else:
+                        user_db = {}
+
+                    # If user is not in the database or the value is False (DMs are closed)
+                    if str(message.author.id) not in user_db or not user_db[str(message.author.id)]:
+                        try:
+                            embed = discord.Embed(title="Фыр!",
+                                                  description="\n**Мы хотели у тебя кое-что попросить :3** \n- Мы были бы очень рады если ты поучаствуешь в развитии сервера Сай и его ФЫР [SWC] ... \n- Всё что нужно сделать - это лишь пройти небольшой опросник по качеству пребывания на сервере\n||(это не займёт больше 5 минут)||",
+                                                  color=discord.Color.blue())
+                            embed.add_field(name="Держи ссылку",
+                                            value="[**Опросник**](https://forms.gle/vhJXtwWFrhTsXLuCA)", inline=False)
+
+                            await message.author.send(embed=embed)
+
+                            # Save this user ID to the database with True value (DMs are open)
+                            user_db[str(message.author.id)] = True
+
+                        except:  # User has a closed DM
+                            print(f"Could not send message to {message.author.id}, possibly due to closed DMs.")
+                            # Save this user ID to the database with False value (DMs are closed)
+                            user_db[str(message.author.id)] = False
+
+                        # Update the database file
+                        with open("users_db.json", "w") as f:
+                            json.dump(user_db, f)
+                """
+
+                # Your existing code
                 if message.content == "GGAB":
                     report_channel = self.get_channel(self.ctx.servers[message.guild.id]["send"])
                     await report_channel.send("Hello World")
